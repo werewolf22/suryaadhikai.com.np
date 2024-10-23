ad343e2600ed6059abc9c9659dc094719068fcbf
werewolf <hero.himal43@gmail.com>
2024-10-23 13:20:11 +0545

    Added My latest portfolio, social icons and copyright information

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- SEO Metadata -->
+    <meta name="description" content="Surya Adhikari - A Web Developer and Blogger from Kathmandu with 6+ years of experience in HTML, CSS, JavaScript, and PHP. Explore my portfolio and blog to discover my latest projects and insights.">
+    <meta name="keywords" content="Surya Adhikari, Web Developer, Software Engineer, Freelance developer Nepal, HTML, CSS, JavaScript, PHP, Blogger, Descriptive writing, Technical writing, Kathmandu, Nepal, Portfolio, Web Apps">
+    <meta name="author" content="Surya Adhikari">
+    
+    <!-- Open Graph / Facebook Metadata -->
+    <meta property="og:title" content="Surya Adhikari - Web Developer Portfolio">
+    <meta property="og:description" content="Explore the web development work, skills and blog of Surya Adhikari, a developer from Kathmandu.">
+    <meta property="og:image" content="images/profile-pic.png">
+    <meta property="og:url" content="https://suryaadhikari.com.np">
+    <meta property="og:type" content="website">
+    
+    <!-- Twitter Metadata -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Surya Adhikari - Web Developer Portfolio">
+    <meta name="twitter:description" content="Explore the web development work, skills and blog of Surya Adhikari, a developer from Kathmandu.">
+    <meta name="twitter:image" content="images/profile-pic.png">
+
+    <link rel="icon" href="images/SURYA-LOGO.png" type="image/png" />
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+
+    <title>Surya Adhikari - Web Developer Portfolio</title>
+</head>
+<body>
+    <header class="navbar">
+        <a href="/">
+            <img src="images/SURYA-LOGO.png" alt="Surya Adhikari Logo">
+        </a>
+        <h1>Surya Adhikari</h1>
+        <a href="blog"><button class="portfolio-button">Blog</button></a>
+    </header>
+    <main>
+        <article>
+            <div class="hero">
+                <div class="skill-flow">
+                    <div class="skill">
+                        <a target="_blank" href="https://github.com/werewolf22/html_css">
+                            HTML
+                        </a>
+                        <div class="animation-frame">
+                            <div class="box-enter"></div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <a target="_blank" href="https://github.com/werewolf22/html_css">
+                            CSS
+                        </a>
+                        <div class="animation-frame">
+                            <div class="box-enter"></div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <a target="_blank" href="https://github.com/werewolf22/js-exercises">
+                            JS
+                        </a>
+                        <div class="animation-frame">
+                            <div class="box-enter"></div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <a target="_blank" href="https://github.com/werewolf22/100-Algorithms-milestone">
+                            PHP
+                        </a>
+                        <div class="animation-frame">
+                            <div class="box-enter"></div>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <img src="images/profile-pic.png" alt="Surya Adhikari - Web Developer">
+
+                <header>
+                    <h1>Welcome! I'm Surya Adhikari, a Web Developer and Blogger</h1>
+                    <p>I have a passion for crafting engaging creative, research intensive and technical content. With over 6 years of experience in web development, I specialize in HTML, CSS, JavaScript, and PHP. Explore my portfolio and blog to discover my latest projects and insights.</p>
+                </header>
+            
+                <!-- About Me Section -->
+                <section id="about">
+                    <h2>About Me</h2>
+                    <p>I am a highly skilled Web Developer with over 6 years of experience in designing, developing, and deploying dynamic web applications. Proficient in PHP, Laravel, JavaScript, and MySQL, with expertise in both front-end and back-end technologies. I am adept at building scalable, secure, and maintainable applications, including custom CMS and SaaS solutions.</p>
+                    <p>With a strong understanding of the software development lifecycle, object-oriented design, and modern web frameworks, I have contributed to various innovative projects. I am also experienced in collaborating with cross-functional teams to deliver optimized, user-friendly systems.</p>
+                    <p>As a problem-solver with solid DevOps and server management skills, I am eager to contribute to innovative projects that push the boundaries of web development.</p>
+                </section>
+            
+                <!-- Key Achievements Section -->
+                <section id="achievements">
+                    <h2>Key Achievements</h2>
+                    <ul>
+                        <li>Successfully led the development of multiple large-scale web applications, including a property management system (HMS) and a travel management system (TMS), driving business growth and efficiency.</li>
+                        <li>Developed and deployed a custom CMS application, ensuring seamless content management and integration with external systems, hosted on Contabo and DataHub servers.</li>
+                        <li>Optimized database performance for business reporting needs, improving system responsiveness and handling large datasets effectively.</li>
+                        <li>Contributed to the creation of a job recommendation system using machine learning algorithms as part of a group project, enhancing user functionality and system performance.</li>
+                        <li>Demonstrated strong debugging and problem-solving skills by resolving critical production issues and enhancing application performance across various projects.</li>
+                    </ul>
+                </section>
+            
+                <!-- Portfolio Section -->
+                <section id="portfolio">
+                    <h2>Portfolio</h2>
+                    <p>Check out some of my latest web development projects below.</p>
+                    <!-- You can add portfolio project cards or links here -->
+                     <a href="https://www.gsenconsultancy.com/" target="_blank"> <button class="portfolio-button"> A consulting Institute's website</button></a>
+                </section>
+
+                <!-- Contact Me Section with Social Media Icons -->
+                <section id="contact">
+                    <h2>Contact Me</h2>
+                    <span>Feel free to reach out to me on social media or send me an email.</span>
+                    <div class="social-icons">
+                        <a href="https://www.facebook.com/surya.adhikari.7583/" target="_blank" class="social-icon">
+                            <i class="fab fa-facebook-f"></i> <!-- Facebook Icon -->
+                        </a>
+                        <a href="https://x.com/werewolf939" target="_blank" class="social-icon">
+                            <i class="fab fa-twitter"></i> <!-- Twitter Icon -->
+                        </a>
+                        <a href="https://www.linkedin.com/in/surya-adhikari-296929118/" target="_blank" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i> <!-- LinkedIn Icon -->
+                        </a>
+                        <a href="https://github.com/werewolf22" target="_blank" class="social-icon">
+                            <i class="fab fa-github"></i> <!-- GitHub Icon -->
+                        </a>
+                        <a href="mailto:hero.himal43@gmail.com" class="social-icon">
+                            <i class="fas fa-envelope"></i> <!-- Email Icon -->
+                        </a>
+                    </div>
+                </section>
+                
+                <div class="portfolio">
+                    <div class="animation-frame">
+                        <div></div>
+                    </div>
+                    <a href="https://github.com/werewolf22" target="_blank"><span>Web Apps</span></a>
+                </div>
+            </div>
+        </article>
+    </main>
+
+    <!-- Footer Section -->
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Surya Adhikari. All rights reserved.</p>
+    </footer>
+    
+    <script src="scripts/script.js"></script>
+</body>
+</html>
